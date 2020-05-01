8728c9774930e6b13b18b2bfc8a34b65c992bbba
sergeyterr <ater123@mail.ru>
2020-05-01 17:52:38 +0300

    Edit icons and change place items and add type url

--- plugin.php
+++ plugin.php
@@ -71,16 +71,6 @@ function oca_option_settings() {
 	// Add section
 	add_settings_section( 'oca_section_1', __( 'First Links', 'oca' ), '', $admin_page_name );
 
-	// Add field 1
-	$field_params = [
-		'type'        => 'text',
-		'id'          => 'src_1',
-		'desc'        => __( 'Input source', 'oca' ),
-		'label_for'   => 'src_1',
-		'option_name' => $options_name
-	];
-	add_settings_field( 'src_1_field', __( 'Input source', 'oca' ), 'oca_option_display_settings', $admin_page_name, 'oca_section_1', $field_params );
-
 	// Add field 2
 	$field_params = [
 		'type'        => 'text',
@@ -91,22 +81,24 @@ function oca_option_settings() {
 	];
 	add_settings_field( 'text_1_field', __( 'Input Text Link', 'oca' ), 'oca_option_display_settings', $admin_page_name, 'oca_section_1', $field_params );
 
+
+	// Add field 1
+	$field_params = [
+		'type'        => 'url',
+		'id'          => 'src_1',
+		'desc'        => __( 'Input source', 'oca' ),
+		'label_for'   => 'src_1',
+		'option_name' => $options_name,
+		'placeholder' => 'https://site.com'
+	];
+	add_settings_field( 'src_1_field', __( 'Input source', 'oca' ), 'oca_option_display_settings', $admin_page_name, 'oca_section_1', $field_params );
+
 	$options_name = 'oca_options_2';
 	//add validation
 	register_setting( 'oca_group', $options_name, 'true_validate_settings' );
 
 	// Add section
 	add_settings_section( 'oca_section_2', __( 'Second Links', 'oca' ), '', $admin_page_name );
-
-	// Add field 1
-	$field_params = [
-		'type'        => 'text',
-		'id'          => 'src_2',
-		'desc'        => __( 'Input source', 'oca' ),
-		'label_for'   => 'src_2',
-		'option_name' => $options_name
-	];
-	add_settings_field( 'src_2_field', __( 'Input source', 'oca' ), 'oca_option_display_settings', $admin_page_name, 'oca_section_2', $field_params );
 
 	// Add field 2
 	$field_params = [
@@ -117,6 +109,17 @@ function oca_option_settings() {
 		'option_name' => $options_name
 	];
 	add_settings_field( 'text_2_field', __( 'Input Text Link', 'oca' ), 'oca_option_display_settings', $admin_page_name, 'oca_section_2', $field_params );
+
+	// Add field 1
+	$field_params = [
+		'type'        => 'url',
+		'id'          => 'src_2',
+		'desc'        => __( 'Input source', 'oca' ),
+		'label_for'   => 'src_2',
+		'option_name' => $options_name,
+        'placeholder' => 'https://site.com'
+	];
+	add_settings_field( 'src_2_field', __( 'Input source', 'oca' ), 'oca_option_display_settings', $admin_page_name, 'oca_section_2', $field_params );
 
 }
 
@@ -134,12 +137,29 @@ function oca_option_display_settings( $args ) {
 	$type      = $args['type'];
 	$desc      = $args['desc'];
 	$label_for = $args['label_for'];
+	$placeholder = isset($args['placeholder']) ? $args['placeholder'] : '';
 	$o         = get_option( $option_name );
 
 	switch ( $type ) {
 		case 'text':
 			$o[ $id ] = esc_attr( stripslashes( $o[ $id ] ) );
-			echo "<input class='regular-text' type='text' id='$id' name='{$option_name}[$id]' value='$o[$id]' />";
+			echo "<input
+			class='regular-text'
+			type='text'
+			id='$id'
+			name='{$option_name}[$id]'
+			placeholder='{$placeholder}'
+			value='$o[$id]' />";
+			echo ( $desc != '' ) ? "<br /><span class='description'>$desc</span>" : "";
+			break;
+		case 'url':
+			$o[ $id ] = esc_attr( stripslashes( $o[ $id ] ) );
+			echo "<input
+			class='regular-text'
+			type='url' id='$id'
+			name='{$option_name}[$id]'
+			placeholder='{$placeholder}'
+			value='$o[$id]' />";
 			echo ( $desc != '' ) ? "<br /><span class='description'>$desc</span>" : "";
 			break;
 	}
@@ -173,11 +193,7 @@ function pojo_toolbar_before_buttons_output() {
         <a
                 href="<?php echo $options['src_1']; ?>">
             <span class="pojo-a11y-toolbar-icon">
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" viewBox="0 0 448 448">
-                    <path fill="currentColor"
-                          d="M256 200v16c0 4.25-3.75 8-8 8h-56v56c0 4.25-3.75 8-8 8h-16c-4.25 0-8-3.75-8-8v-56h-56c-4.25 0-8-3.75-8-8v-16c0-4.25 3.75-8 8-8h56v-56c0-4.25 3.75-8 8-8h16c4.25 0 8 3.75 8 8v56h56c4.25 0 8 3.75 8 8zM288 208c0-61.75-50.25-112-112-112s-112 50.25-112 112 50.25 112 112 112 112-50.25 112-112zM416 416c0 17.75-14.25 32-32 32-8.5 0-16.75-3.5-22.5-9.5l-85.75-85.5c-29.25 20.25-64.25 31-99.75 31-97.25 0-176-78.75-176-176s78.75-176 176-176 176 78.75 176 176c0 35.5-10.75 70.5-31 99.75l85.75 85.75c5.75 5.75 9.25 14 9.25 22.5z" "="">
-                    </path>
-                </svg>
+                <span class="dashicons dashicons-media-text"></span>
             </span>
             <span class="pojo-a11y-toolbar-text"><?php echo $options['text_1']; ?></span>
         </a>
@@ -200,11 +216,7 @@ function pojo_toolbar_after_buttons_output() {
         <a
                 href="<?php echo $options['src_2']; ?>">
             <span class="pojo-a11y-toolbar-icon">
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" viewBox="0 0 448 448">
-                    <path fill="currentColor"
-                          d="M256 200v16c0 4.25-3.75 8-8 8h-56v56c0 4.25-3.75 8-8 8h-16c-4.25 0-8-3.75-8-8v-56h-56c-4.25 0-8-3.75-8-8v-16c0-4.25 3.75-8 8-8h56v-56c0-4.25 3.75-8 8-8h16c4.25 0 8 3.75 8 8v56h56c4.25 0 8 3.75 8 8zM288 208c0-61.75-50.25-112-112-112s-112 50.25-112 112 50.25 112 112 112 112-50.25 112-112zM416 416c0 17.75-14.25 32-32 32-8.5 0-16.75-3.5-22.5-9.5l-85.75-85.5c-29.25 20.25-64.25 31-99.75 31-97.25 0-176-78.75-176-176s78.75-176 176-176 176 78.75 176 176c0 35.5-10.75 70.5-31 99.75l85.75 85.75c5.75 5.75 9.25 14 9.25 22.5z" "="">
-                    </path>
-                </svg>
+                <span class="dashicons dashicons-email-alt"></span>
             </span>
             <span class="pojo-a11y-toolbar-text"><?php echo $options['text_2']; ?></span>
         </a>
